ac5be02cd6d53ee937e78dbac5175becde2ac3ff
twolffgramm <69900545+twolffgramm@users.noreply.github.com>
2021-08-26 21:01:41 +0200

    Add files via upload

--- SimulationPanel.java
+++ SimulationPanel.java
@@ -1,5 +1,6 @@
 package MoverAttractor;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
@@ -29,14 +30,13 @@ public class SimulationPanel extends JPanel implements Runnable
   Image image;
   Graphics graphics;
   Random random;
-  Mover mover;
-  ArrayList<Mover> movers = new ArrayList<>();
-  Attractor attractor;
+  ArrayList<Attractor> attractors = new ArrayList<>();
+  ArrayList<Attractor> attractorsWithout = new ArrayList<>();
+  Vector2D centerOfMass = new Vector2D();
 
   SimulationPanel()
   {
-    newAttractor();
-    newMover();
+    newAttractors();
     this.setFocusable(true);
     this.setPreferredSize(SCREEN_SIZE);
 
@@ -47,24 +47,21 @@ public class SimulationPanel extends JPanel implements Runnable
   /**
    * 
    */
-  public void newAttractor()
+  public void newAttractors()
   {
-    attractor = new Attractor(SIMULATION_WIDTH / 2, SIMULATION_HEIGHT / 2, 500);
-  }
-
-  /**
-   * 
-   */
-  public void newMover()
-  {
-    random = new Random();
-    // mover = new Mover(random.nextInt(SIMULATION_WIDTH), random.nextInt(SIMULATION_HEIGHT), 100);
-    for (int i = 0; i <= 10; i++)
+  	random = new Random();
+  	int[] positionX = {SIMULATION_WIDTH / 2, SIMULATION_WIDTH / 2};
+  	int[] positionY = {SIMULATION_HEIGHT / 2 - 100, SIMULATION_HEIGHT / 2 +100};
+  	int[] velX = {-2,2};
+  	int[] velY = {0,0};
+  	
+    for (int i = 0; i <= 1; i++)
     {
-      movers
-          .add(new Mover(random.nextInt(SIMULATION_WIDTH), random.nextInt(SIMULATION_HEIGHT), 100));
+//      attractors
+//          .add(new Attractor(random.nextInt(SIMULATION_WIDTH), random.nextInt(SIMULATION_HEIGHT), 100));
+    	attractors
+    				.add(new Attractor(positionX[i], positionY[i], 100, velX[i], velY[i]));
     }
-    // mover = new Mover(SIMULATION_WIDTH / 2, SIMULATION_HEIGHT / 2 + 300, 100);
   }
 
   @Override
@@ -81,16 +78,17 @@ public class SimulationPanel extends JPanel implements Runnable
    */
   public void draw(Graphics g)
   {
-    attractor.draw(g);
-    for (int i = 0; i <= movers.size() - 1; i++)
+    for (int i = 0; i <= attractors.size() - 1; i++)
     {
-      movers.get(i)
-          .drawLines(g);
-      movers.get(i)
-          .draw(g);
+    	attractors.get(i).draw(g);
     }
-    // mover.drawLines(g);
-    // mover.draw(g);
+    
+    g.setColor(Color.RED);
+    int r = 5;
+    int x = (int) centerOfMass.getX() - r / 2;
+    int y = (int) centerOfMass.getY() - r / 2;
+    g.fillOval(x, y, r, r);
+    
     Toolkit.getDefaultToolkit()
         .sync();
   }
@@ -100,15 +98,30 @@ public class SimulationPanel extends JPanel implements Runnable
    */
   public void move()
   {
-    attractor.move();
-    for (int i = 0; i <= movers.size() - 1; i++)
-    {
-      movers.get(i)
-          .move();
-      attractor.attract(movers.get(i));
-    }
-    // mover.move();
-    // attractor.attract(mover);
+  	for (int i = 0; i <= attractors.size() - 1; i++) {
+  		attractors.get(i).attract(attractors,i);
+  		attractors.get(i).move();
+  	}
+  	
+  	double totalMass = 0;
+  	Vector2D position;
+  	double mass;
+  	Vector2D velocity;
+  	Vector2D momentum = new Vector2D();
+  	double totalMomentum;
+  	for (int i = 0; i <= attractors.size() - 1; i++) {
+  		totalMass += attractors.get(i).mass;
+  		position = attractors.get(i).position;
+  		velocity = attractors.get(i).velocity;
+  		mass = attractors.get(i).mass;
+  		momentum = momentum.add(velocity.multiply(mass));
+  		centerOfMass = centerOfMass.add(position.multiply(mass));
+  	}
+  	centerOfMass = centerOfMass.multiply(1 / totalMass);
+  	totalMomentum = Vector2D.magnitude(momentum);
+  	System.out.println(totalMomentum);
+  	
+  	
   }
 
   @Override
